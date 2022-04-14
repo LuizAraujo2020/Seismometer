8e63c7db1d76c87afc2c773aa25810a609009c7f
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2022-04-14 00:08:38 -0300

    Seismometer Browser

--- SeismometerApp.swift
+++ SeismometerApp.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 @main
-struct MyApp: App {
+struct SeismometerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SeismometerBrowser()
         }
     }
 }
